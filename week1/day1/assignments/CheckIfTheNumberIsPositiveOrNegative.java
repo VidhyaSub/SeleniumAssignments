b9c632a179925ac9b0d8653941154e4279bf3eac
VidhyaSub <110732914+VidhyaSub@users.noreply.github.com>
2022-08-07 18:01:12 +0530

    Update CheckIfTheNumberIsPositiveOrNegative.java

--- week1/day1/assignments/CheckIfTheNumberIsPositiveOrNegative.java
+++ week1/day1/assignments/CheckIfTheNumberIsPositiveOrNegative.java
@@ -4,17 +4,16 @@ public class CheckIfTheNumberIsPositiveOrNegative {
 public static void main(String[] args) {
 	
 	//Problem statement: Check if the number is positive or negative
-	/*Pseudocode
-	 * : ---------- 
-	 * 1. Initialize a number, say, int number= 35; 
-	 * 
-	 * 2. If a number is positive, print "The number is positive" 
-	 * 
-	 * If a number is negative, print "The number is negative" 
-	 * 
-	 * If it nether negative nor positive, print as
-	 * 
-	 * "The number is neither positive nor negative"
-	 */
+	
+	int num = 1000;
+		if (a>0) {
+			System.out.println("The number is positive");
+		}
+		else if (a<0) {
+			System.out.println("The number is negative");
+		}
+		else {
+			System.out.println("The number is neither positive nor negative");
+		}
 }
 }
